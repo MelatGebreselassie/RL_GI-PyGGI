0b98a40fd510689b3454e6d3fd81f0df50418815
MelatGebreselassie <54198079+MelatGebreselassie@users.noreply.github.com>
2022-08-05 16:16:12 +0300

    add artefacts

---
+++ pyggi/tevc_artefacts/artefact/code/minisat-2.2.0/simp/depend.mk
@@ -0,0 +1,12 @@
+/Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/Main.o /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/Main.or /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/Main.od /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/Main.op: \
+  /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/Main.cc \
+  ../utils/System.h ../mtl/IntTypes.h ../utils/ParseUtils.h \
+  ../utils/Options.h ../mtl/Vec.h ../mtl/XAlloc.h ../core/Dimacs.h \
+  ../core/SolverTypes.h ../mtl/Alg.h ../mtl/Map.h ../mtl/Alloc.h \
+  ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h
+/Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/SimpSolver.o /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/SimpSolver.or /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/SimpSolver.od /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/SimpSolver.op: \
+  /Users/melathaile/Documents/Running_PyGGI/pyggi_moead/pyggi/artefacts/artefact/code/minisat-2.2.0/simp/SimpSolver.cc \
+  ../mtl/Sort.h ../mtl/Vec.h ../mtl/IntTypes.h ../mtl/XAlloc.h \
+  ../simp/SimpSolver.h ../mtl/Queue.h ../core/Solver.h ../mtl/Heap.h \
+  ../mtl/Alg.h ../utils/Options.h ../utils/ParseUtils.h \
+  ../core/SolverTypes.h ../mtl/Map.h ../mtl/Alloc.h ../utils/System.h
